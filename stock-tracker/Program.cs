79696629b61b306f35035d783a71c08f7d281c5b
Faesel Saeed <faesel@gmail.com>
2023-08-05 16:17:13 +0100

    Added more metrics

--- stock-tracker/Program.cs
+++ stock-tracker/Program.cs
@@ -44,16 +44,21 @@ foreach(var stock in stocks)
 
             table.AddRow(ColouredColumn.GetColouredColumn("Total Revenue", incomeStatement.annualReports.Select(x => decimal.Parse(x.totalRevenue)).Take(5).ToList()));
             table.AddRow(ColouredColumn.GetColouredColumn("Cost of Revenue", incomeStatement.annualReports.Select(x => decimal.Parse(x.costOfRevenue)).Take(5).ToList()));
+            table.AddRow(ColouredColumn.GetColouredColumn("Cost of Revenue", incomeStatement.annualReports.Select(x => decimal.Parse(x.costofGoodsAndServicesSold)).Take(5).ToList()));
             table.AddRow(ColouredColumn.GetColouredColumn("Gross Profit", incomeStatement.annualReports.Select(x => decimal.Parse(x.grossProfit)).Take(5).ToList()));
             table.AddRow("[underline]Operating Expenses[/]", "[yellow]-[/]", "[yellow]-[/]", "[yellow]-[/]", "[yellow]-[/]");
             table.AddRow(ColouredColumn.GetColouredColumn("> Research & Development", incomeStatement.annualReports.Select(x => decimal.Parse(x.researchAndDevelopment)).Take(5).ToList()));
             table.AddRow(ColouredColumn.GetColouredColumn("> Selling general & Administrative", incomeStatement.annualReports.Select(x => decimal.Parse(x.sellingGeneralAndAdministrative)).Take(5).ToList()));
+            table.AddRow(ColouredColumn.GetBoldColouredColumn("> Operating Expenses", incomeStatement.annualReports.Select(x => decimal.Parse(x.operatingExpenses)).Take(5).ToList()));
             table.AddRow(ColouredColumn.GetBoldColouredColumn("> Total Operating Expenses", incomeStatement.annualReports.Select(x => decimal.Parse(x.researchAndDevelopment) + decimal.Parse(x.sellingGeneralAndAdministrative)).Take(5).ToList()));
             table.AddRow(ColouredColumn.GetBoldColouredColumn("Operating Income or Loss", incomeStatement.annualReports.Select(x => decimal.Parse(x.grossProfit) - decimal.Parse(x.researchAndDevelopment) - decimal.Parse(x.sellingGeneralAndAdministrative)).Take(5).ToList()));
             table.AddRow(ColouredColumn.GetColouredColumn("Interest Expense", incomeStatement.annualReports.Select(x => decimal.Parse(x.interestExpense)).Take(5).ToList()));
-            table.AddRow(ColouredColumn.GetColouredColumn("Total other income/expenses net", incomeStatement.annualReports.Select(x => decimal.Parse(x.otherNonOperatingIncome)).Take(5).ToList()));
+            table.AddRow(ColouredColumn.GetColouredColumn("Income before tax", incomeStatement.annualReports.Select(x => decimal.Parse(x.incomeBeforeTax)).Take(5).ToList()));
+            table.AddRow(ColouredColumn.GetColouredColumn("Income tax expense", incomeStatement.annualReports.Select(x => decimal.Parse(x.incomeTaxExpense)).Take(5).ToList()));
+            table.AddRow(ColouredColumn.GetColouredColumn("Income from continuing operations", incomeStatement.annualReports.Select(x => decimal.Parse(x.netIncomeFromContinuingOperations)).Take(5).ToList()));
+            table.AddRow(ColouredColumn.GetBoldColouredColumn("Net income", incomeStatement.annualReports.Select(x => decimal.Parse(x.netIncome)).Take(5).ToList()));
+            table.AddRow(ColouredColumn.GetColouredColumn("EBITDAH", incomeStatement.annualReports.Select(x => decimal.Parse(x.ebitda)).Take(5).ToList()));
             
-
             AnsiConsole.Write(table);
         }
 
